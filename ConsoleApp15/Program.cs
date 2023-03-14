c52cc48bf1f461c2a04abce51cc890c301534f45
collegePIS <127884424+collegePIS@users.noreply.github.com>
2023-03-14 19:01:08 +0100

    Update Program.cs
    <3

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp15
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Novo!");
+            Console.WriteLine("Novo! <3 <3 <3");
         }
     }
 }
